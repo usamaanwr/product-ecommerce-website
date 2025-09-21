acac845dee82cf984aeaefd9dd553bfb935edab5
Muhammad Osama <anweru98@gmail.com>
2025-09-22 00:20:06 +0500

    fake store api is fetch and new functionalty use in js

---
+++ JS/product_detail.js
@@ -0,0 +1,33 @@
+const detailurl = `https://fakestoreapi.com/products`;
+const productDetail = document.getElementById("product_detail");
+document.addEventListener("DOMContentLoaded", async () =>{
+    const params = new URLSearchParams(window.location.search);
+    const productId = params.get("id");
+    console.log(productId);
+    if (productId) {
+        try {
+            const product_data = await fetch(`${detailurl}/${productId}`);
+            const detail_data = await product_data.json()
+            console.log(detail_data);
+            
+            productDetail.innerHTML = `<div class="product_detail"">
+  <img src="${detail_data.image}" class="card-img-top" alt="...">
+  <hr>
+  <div class="card-body">
+    <h5 class="card-title">Price =${detail_data.price}</h5>
+    <h5 class="card-title">${detail_data.category}</h5>
+    <p class="card-text">S${detail_data.description}</p>
+    <h5>Title</h5>
+    <p class="card-text">S${detail_data.title}</p>
+    <div class ="rating_detail">
+<p>Rating ${detail_data.rating.rate}</p>
+<p>Order ${detail_data.rating.count}</p>
+</div>
+  </div>
+</div>`
+        } catch (error) {
+            console.error(error);
+            
+        }
+    }
+})
